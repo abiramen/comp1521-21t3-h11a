3c933dce4eb1eb3871587d31adb0486941a81440
abiramen <7523422+abiramen@users.noreply.github.com>
2021-09-16 00:15:42 +1000

    week1: Add content

---
+++ week01/factorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int factorial(int n);
+
+int main(void) {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("%d! = %d\n", n, factorial(n));
+}
+
+int factorial(int n) {
+    // TODO
+}
---
+++ week01/static.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int counter() {
+    int count;
+}
+
+int main() {
+    printf("%d\n", counter());
+    printf("%d\n", counter());
+    printf("%d\n", counter());
+    printf("%d\n", counter());
+}
