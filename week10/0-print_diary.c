9c1427b043b404eeef74e104f1387b5c960da014
abiramen <7523422+abiramen@users.noreply.github.com>
2021-11-18 04:04:48 +1100

    week10: Add files before tute

---
+++ week10/0-print_diary.c
@@ -0,0 +1,17 @@
+/**
+ * Write a C program, print_diary.c,
+ * which prints the contents of the file $HOME/.diary to stdout.
+ * 
+ * Be sure to handle any errors!
+ * snprintf is a convenient function for constructing the pathname of the diary file.
+ * (it's also pretty handy for subset3 of a certain assignment.)
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+    // TODO: write some code
+    
+    return 0;
+}
